041eed77036d76aaa56bd4696c1d6e22f42dc5e3
sakibhosen98 <sakibhosen9815@gmail.com>
2023-06-11 19:01:57 +0600

    added users

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1w56ggc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +34,7 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
       console.log(user)
@@ -45,6 +46,20 @@ async function run() {
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    })
+
+    app.patch('users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
     })
 
     // instructions
