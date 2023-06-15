089ce7083d15cb36e472f3d3494d3a91f05f817e
sakibhosen98 <sakibhosen9815@gmail.com>
2023-06-15 17:04:54 +0600

    added classes pending approved deny by classesCollection

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1w56ggc.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -39,6 +57,17 @@ async function run() {
 
       res.send({token})
     })
+
+    // verify Admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
 
     // users related apis
     app.get('/users', async(req, res) => {
@@ -76,32 +105,17 @@ async function run() {
     })
 
     // manage classes peanding, approve, deny
-    app.patch('/classes/pending/:id', async (req, res) => {
-      const id = req.params.id;
-      console.log(id)
-      const filter = {_id: new ObjectId(id)};
-      const updateDoc = {
-        $set: {
-          role: 'pending'
-        },
-      };
-
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
-
-    })
-
     app.patch('/classes/approved/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(id)
+      console.log('id',id)
       const filter = {_id: new ObjectId(id)};
       const updateDoc = {
         $set: {
-          role: 'approved'
+          roles: 'approved'
         },
       };
 
-      const result = await usersCollection.updateOne(filter, updateDoc);
+      const result = await classesCollection.updateOne(filter, updateDoc);
       res.send(result);
 
     })
@@ -112,11 +126,11 @@ async function run() {
       const filter = {_id: new ObjectId(id)};
       const updateDoc = {
         $set: {
-          role: 'deny'
+          roles: 'deny'
         },
       };
 
-      const result = await usersCollection.updateOne(filter, updateDoc);
+      const result = await classesCollection.updateOne(filter, updateDoc);
       res.send(result);
 
     })
